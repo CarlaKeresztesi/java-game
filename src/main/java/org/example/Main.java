c42532f51b0383fd973ce5e578cdd7be81e2a1bc
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-02-12 22:44:18 +0000

    Added greeting and welcome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\u2665  \u2663  \u2666  \u2660  \u2665  \u2663  \u2666  \u2660");
+
+        System.out.println("Hello, Friend!");
+
+        System.out.println("Welcome to \u2665 \u2663 \u2666 \u2660 Snap \u2665 \u2663 \u2666 \u2660!");
+
 
 
 
